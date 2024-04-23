927432c13d199a4643aa415e7e235d4d8d1b38ea
NAGARAJ S <nagaraj516700@gmail.com>
2024-04-23 22:09:49 +0530

    Feat : Added DB Connection

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
-const express = require('express')
+const express = require('express');
 const app = express();
-const dotenv = require('dotenv')
-dotenv.config();
+const dotenv = require('dotenv');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 const PORT = process.env.PORT || 8000;
+dotenv.config();
+const corsOption = {
+   origin:true,
+   credential:true,
+}
+
+//DB Connection
+
+let isDBConnected = false;
+const connectDb = async()=> {
+   try{
+      await mongoose.connect(process.env.MONGODB_URL)
+      console.log('DB Connected');
+      isDBConnected = true;
+   }
+   catch(err){
+      isDBConnected = false;
+   }
+}
+
+//middlewares
+app.use(express.json());
+app.use(cors(corsOption));
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+connectDb()
 
 app.get('/api/v1/', (req,res)=> {
-   res.status(200).json({message:"Working..."})
+   res.status(200).json({message:"Working...",db:isDBConnected});
 })
 
 
